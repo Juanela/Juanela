c6f97013f43f2ae893f683bf53f18675e51d621b
Duoc UC <Duoc UC@B33-15>
2017-12-19 19:35:30 -0300

    bla

--- src/proyectouno/PROYECTOUNO.java
+++ src/proyectouno/PROYECTOUNO.java
@@ -14,9 +14,9 @@ public class PROYECTOUNO {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         // TODO code application logic here
         //una estupidez
-    }
+    }*/
     
 }
